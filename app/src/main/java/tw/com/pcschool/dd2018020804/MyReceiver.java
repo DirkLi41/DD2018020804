c0247c53befc54e16c9c4fedd71613cc88ac1fa8
Jhan <Jhan@gmail.com>
2018-02-09 14:10:02 +0800

    2018020804

---
+++ app/src/main/java/tw/com/pcschool/dd2018020804/MyReceiver.java
@@ -0,0 +1,18 @@
+package tw.com.pcschool.dd2018020804;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+
+public class MyReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        // TODO: This method is called when the BroadcastReceiver is receiving
+        // an Intent broadcast.
+//        throw new UnsupportedOperationException("Not yet implemented");
+
+        Log.d("REC", "DD2018020804 : onReceive");
+    }
+}
